d9ac2f04d9a98b60c7812910c6f2c4d887b555bf
DylanB <dylanbergmann25@gmail.com>
2023-08-22 08:28:16 +0700

    refactored tweet liking

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,7 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  has_many :likes
+  has_many :likes, dependent: :destroy
+  has_many :liking_users, through: :likes, source: :user
 
   validates :body, presence: true, length: { maximum: 280 }
 end
--- spec/presenters/tweet_presenter_spec.rb
+++ spec/presenters/tweet_presenter_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe TweetPresenter, type: :model do
       it "displays the shortended date format" do
         tweet = create(:tweet)
         tweet.update! created_at: 2.days.ago
-        expect(TweetPresenter.new(tweet).created_at).to eq("Sep 1")
+        expect(TweetPresenter.new(tweet: tweet, current_user: build_stubbed(:user)).created_at).to eq("Sep 1")
       end
     end
 
@@ -24,7 +24,7 @@ RSpec.describe TweetPresenter, type: :model do
       it "displays how many hours have past" do
         tweet = create(:tweet)
         tweet.update! created_at: 2.hours.ago
-        expect(TweetPresenter.new(tweet).created_at).to eq("about 2 hours")
+        expect(TweetPresenter.new(tweet: tweet, current_user: build_stubbed(:user)).created_at).to eq("about 2 hours")
       end
     end
   end

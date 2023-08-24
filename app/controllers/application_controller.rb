96068936c9b6db18e101b25f602283fe3736c308
DylanB <dylanbergmann25@gmail.com>
2023-08-24 08:56:22 +0700

    hooked up the actions link in a tweet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,18 @@ class ApplicationController < ActionController::Base
   def redirect_to_username_form
     redirect_to new_username_path
   end
+
+  def previous_path
+    @previous_path ||= Rails.application.routes.recognize_path(request.referrer)
+  end
+
+  def previous_controller_name
+    previous_path[:controller]
+  end
+  helper_method :previous_controller_name # helper_method helps you accessing these methods from the views
+
+  def previous_action_name
+    previous_path[:action]
+  end
+  helper_method :previous_action_name
 end

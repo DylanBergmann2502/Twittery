beda549906d4818ed58461e38303ef0dd1802117
DylanB <dylanbergmann25@gmail.com>
2023-08-29 08:22:38 +0700

    rendered bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
     resources :reply_tweets, only: [:create]
   end
 
+  resources :bookmarks, only: [:index]
+
   get :dashboard, to: "dashboard#index"
   get :profile, to: "profile#show"
   put :profile, to: "profile#update"

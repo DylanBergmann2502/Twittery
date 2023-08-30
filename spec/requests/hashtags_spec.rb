bc08ef7b03d1b61e30b6b4e38fb463d25c1738a6
DylanB <dylanbergmann25@gmail.com>
2023-08-30 07:59:18 +0700

    set up hashtags controller

---
+++ spec/requests/hashtags_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Hashtags", type: :request do
+  let(:user) { create(:user) }
+  let(:tweet) { create(:tweet) }
+
+  before { sign_in user }
+
+  describe "GET index" do
+    it "succeeds" do
+      get hashtags_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST create" do
+    it "creates a new bookmark" do
+      expect do
+        post tweet_bookmarks_path(tweet)
+      end.to change { Bookmark.count }.by(1)
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+
+  describe "DELETE destroy" do
+    it "deletes a bookmark" do
+      bookmark = create(:bookmark, user: user, tweet: tweet)
+      expect do
+        delete tweet_bookmark_path(tweet, bookmark)
+      end.to change { Bookmark.count }.by(-1)
+      expect(response).to have_http_status(:redirect)
+    end
+  end
+end

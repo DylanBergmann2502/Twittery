8e8990e8195879ba304c021c49a632cbded5368e
DylanB <dylanbergmann25@gmail.com>
2023-08-31 08:00:50 +0700

    created messages page

---
+++ spec/requests/message_threads_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Message Threads", type: :request do
+  let(:user) { create(:user) }
+  let(:user_2) { create(:user) }
+
+  before { sign_in user }
+
+  describe "GET index" do
+    it "succeeds" do
+      get message_threads_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+end

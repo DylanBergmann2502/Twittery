8ecbb5c7bbc3b62a4a6d5273f242c6855fddcc2a
DylanB <dylanbergmann25@gmail.com>
2023-09-05 08:17:06 +0700

    paginated through tweets in dashboard

---
+++ app/javascript/controllers/dashboard_tweets_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="dashboard-tweets"
+export default class extends Controller {
+
+  headers = { 'Accept': 'text/vnd.turbo-stream.html' };
+
+  connect() {
+    window.addEventListener('scroll', () => {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+
+        if (this.element.dataset.lastPage === 'false') {
+          fetch(`/dashboard?page=${this.element.dataset.nextPage}`, { headers: this.headers })
+            .then(response => response.text())
+            .then(html => Turbo.renderStreamMessage(html));
+        }
+      }
+    });
+  }
+}
